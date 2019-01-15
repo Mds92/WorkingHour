6dc6e63808bc0df4aaab7f4ecac2a52196ec7717
Mohammmad Dayyan <Mds92@users.noreply.github.com>
2019-01-15 16:22:10 +0330

    افزایش زمان بیکاری به 5 دقیقه

--- WorkingHour/Assets/Constants.cs
+++ WorkingHour/Assets/Constants.cs
@@ -4,7 +4,7 @@ namespace WorkingHour.Assets
 {
     public static class Constants
     {
-        public static TimeSpan MinTimeSpanToIdentifyIdle = new TimeSpan(0, 0, 2, 0);
+        public static TimeSpan MinTimeSpanToIdentifyIdle = new TimeSpan(0, 0, 5, 0);
         public static readonly string RootNodeName = "Database";
         public static readonly string ProjectsNodeName = "Projects";
         public static readonly string ProjectNodeName = "Project";
